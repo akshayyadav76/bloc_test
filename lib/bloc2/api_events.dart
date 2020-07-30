d2293ea862df1b5efb260dc8fcacba404fd06f16
akshayyadav76 <akshayyadav7613@gmail.com>
2020-07-30 13:34:58 +0530

    as

--- lib/bloc2/api_events.dart
+++ lib/bloc2/api_events.dart
@@ -1,5 +1,9 @@
+import 'package:bloc_test/bloc2/api_state.dart';
+
 import './api_bloc.dart';
 import 'package:http/http.dart'as http;
+import 'dart:convert';
+import '../model/api_facts.dart';
 
 abstract class ApiEvents {}
 
@@ -16,17 +20,19 @@ abstract class ApiEvents {}
        const _url = 'https://cat-fact.herokuapp.com/facts';
        bloc.add(Loading());
       var response = await http.get(_url);
-      
-
-
-       
+      if(response.statusCode ==200){
+     Map<String, dynamic> decodedItems = json.decode(response.body);
+      final catFacts = List.from(decodedItems['all']).map((json) => ApiFacts.fromJson(json)).toList();
+      bloc.add(ApiSuccess(catFacts));
+     }     
    }
- 
-  
-  
-
  }
 
  class Loading extends ApiEvents{
   
+ }
+
+ class ApiSuccess extends ApiEvents{
+   ApiSuccess(List allData)
+   
  }
