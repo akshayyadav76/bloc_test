ddbb373341c622e212c4f4bfa1cf1112b5ea1e5d
akshayyadav76 <akshayyadav7613@gmail.com>
2020-07-30 13:19:11 +0530

    aws

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc2/api_bloc.dart';
+import '../bloc2/api_events.dart';
+import '../bloc2/api_state.dart';
 
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var objBloc = 
     return Scaffold(
       appBar: AppBar(title: Text("api bloc 2"),
       actions: <Widget>[
         Text("0",textAlign: TextAlign.center,style: TextStyle(fontSize: 25))
       ],),
-      body: Column(
-        
-      ),
+      body: BlocBuilder<ApiBloc2,ApiState>(
+
+         builder: (context,mystate){
+           if(mystate.dataCheck == Check.Loading){
+             return Center(child:CircularProgressIndicator());
+           }
+         }
+      )
     );
   }
 }
