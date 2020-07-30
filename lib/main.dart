35cab97064e0b913ff3a304f46751417f5bbfa19
akshayyadav76 <akshayyadav7613@gmail.com>
2020-07-30 12:16:32 +0530

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import "package:flutter_bloc/flutter_bloc.dart";
 
-
+import './screen/home_screen.dart';
 
 
 void main() {
@@ -13,12 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Text("d")
+      home: HomeScreen()
     );
   }
 }
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("api bloc 2"),
+      actions: <Widget>[
+        Text("0",textAlign: TextAlign.center,style: TextStyle(fontSize: 25))
+      ],),
+    );
+  }
+}
